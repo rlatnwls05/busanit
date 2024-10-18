a2710d11c9ba7875f791385d16f3d45d55fb7971
rlatnwls05 <90299406+rlatnwls05@users.noreply.github.com>
2024-10-18 16:53:52 +0900

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,94 +1,6 @@
 $(document).ready(function () {
 
 
-  $('.mgnb_bg').hide();
-  $('.mgnb_wrap').hide();
-
-  $('.hg_but').click(function () {
-    $('.mgnb_bg').fadeIn();
-    $('.mgnb_wrap').slideDown();
-  });
-
-
-  $('header .mgnb_wrap .close').click(function () {
-    $('.mgnb_bg').fadeOut();
-    $('.mgnb_wrap').slideUp();
-  });
-
-
-  $('header .mgnb li').click(function () {
-    $('.mgnb_bg').fadeOut();
-    $('.mgnb_wrap').fadeOut();
-  });
-
-
-
-
-
-
-  //스크롤 시 fix class로 바뀌는 //
-  $(window).scroll(function () {
-    if ($(window).scrollTop() >= 700) {
-      $('#header').addClass('fix');
-      $("#header .inner").css({
-        "border": "none"
-      });
-    }
-    else {
-      $('#header').removeClass('fix');
-      $("#header .inner").css({
-        "border-bottom": "1px solid #000"
-      });
-    }
-  })
-
-
-  $('.button_posi p').hide();
-
-  $('.grid_depth li').mouseenter(function () {
-    $(this).find('.button_posi p').fadeIn();
-  });
-  $('.grid_depth li').mouseleave(function () {
-    $(this).find('.button_posi p').fadeOut();
-  });
-
-
-
-  $('#web_project .content_wrap .button_txt').hide();
-
-  $('#web_project li').mouseenter(function () {
-    $(this).find('.button_txt').fadeIn();
-  });
-  $('#web_project li').mouseleave(function () {
-    $(this).find('.button_txt').fadeOut();
-  });
-
-
-  $('#about_me .progress_bar').hide();
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 1100) {
-      $('#about_me .progress_bar').fadeIn();
-      $('#about_me .bar1 , #about_me .bar2').animate({
-        width: '95%'
-      }, 1000);
-      $('#about_me .bar3').animate({
-        width: '80%'
-      }, 1000);
-      $('#about_me .bar4').animate({
-        width: '70%'
-      }, 1000);
-      $('#about_me .bar5').animate({
-        width: '80%'
-      }, 1000);
-    }
-  });
-
-
-
-
-
-
-//  내가 작성
   $(".mvis_slick").slick({
         // lazyLoad: 'ondemand', // ondemand progressive anticipated
         infinite: true,
@@ -164,7 +76,7 @@ $(document).ready(function () {
             });
         });
       }
-
+      
       // 배경 이미지를 변경하는 함수
       function changeBackground(href) {
           var bgImageMap = {
@@ -178,6 +90,30 @@ $(document).ready(function () {
       }
 
       setupTabs(); // 탭 설정 함수 호출
+
+
+      
+       // 모바일 확인 함수
+       function isMobile() {
+        return $(window).width() <= 560;
+        }
+
+        // 각 버튼 클릭 이벤트 설정
+        $('.moreBut').on('click', function(event) {
+            event.preventDefault(); // 기본 링크 동작 방지
+            let id = $(this).data('id'); // data-id 속성에서 ID 가져오기
+
+            // 모바일일 경우 다른 URL 설정
+            let url;
+            if (isMobile()) {
+                url = `https://btc.ac.kr/m/?j=${id}`;
+            } else {
+                url = `https://btc.ac.kr/p/?j=${id}`;
+            }
+
+            window.open(url, '_blank'); // 새 탭에서 링크 열기
+        });
+
 
       // ********footer 팝업
       const modal = document.getElementById("myModal");
